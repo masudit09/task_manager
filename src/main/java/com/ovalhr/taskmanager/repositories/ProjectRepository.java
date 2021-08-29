ab76fab1a92fe66c220131f058bb50d287af1d26
Masud Rana <masud.it09@gmail.com>
2021-08-30 02:19:26 +0600

    user access implemented on project api

--- src/main/java/com/ovalhr/taskmanager/repositories/ProjectRepository.java
+++ src/main/java/com/ovalhr/taskmanager/repositories/ProjectRepository.java
@@ -2,11 +2,17 @@ package com.ovalhr.taskmanager.repositories;
 
 import com.ovalhr.taskmanager.entity.Project;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 /**
  * Created by rana on 8/27/21.
  */
 public interface ProjectRepository extends PagingAndSortingAndSpecificationExecutorRepository<Project> {
+    @Query("SELECT x FROM Project x WHERE x.createdBy = :username AND x.id=:id")
+    public Optional<Project> findOwnProjectById(@Param("id") Long id, @Param("username") String username);
 
+    @Query("SELECT x FROM Project x WHERE x.createdBy = :username")
+    public List<Project> findAllByUserName(@Param("username") String username);
 }
--- src/main/java/com/ovalhr/taskmanager/web/resource/ProjectController.java
+++ src/main/java/com/ovalhr/taskmanager/web/resource/ProjectController.java
@@ -4,6 +4,7 @@ import com.ovalhr.taskmanager.entity.Project;
 import com.ovalhr.taskmanager.mapper.Response;
 import com.ovalhr.taskmanager.repositories.ProjectRepository;
 import com.ovalhr.taskmanager.repositories.UserRepository;
+import com.ovalhr.taskmanager.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -26,9 +27,6 @@ public class ProjectController {
     @Autowired
     private ProjectRepository projectRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Page> paginationList(@RequestParam( value = "pageNumber", required = false) Integer pageNumber) {
         if(pageNumber== null) {
@@ -47,45 +45,78 @@ public class ProjectController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Response> update(@Valid @RequestBody Project newProject, @PathVariable Long id) {
-        return projectRepository.findById(id) .map(project -> {
-                    project.setName(newProject.getName());
-            return new ResponseEntity<Response>(new Response(projectRepository.save(project)), HttpStatus.OK);
-        }).orElseGet(() -> {
-            return new ResponseEntity<Response>(new Response("Project Not found with given id.", null), HttpStatus.OK);
-        });
+        if(Util.hasRole("ROLE_ADMIN")) {
+            return projectRepository.findById(id) .map(project -> {
+                project.setName(newProject.getName());
+                return new ResponseEntity<Response>(new Response(projectRepository.save(project)), HttpStatus.OK);
+            }).orElseGet(() -> {
+                return new ResponseEntity<Response>(new Response("Project Not found of don't have access with given id.", null), HttpStatus.OK);
+            });
+        } else {
+            return projectRepository.findOwnProjectById(id, Util.getCurrentUsername()).map(project -> {
+                project.setName(newProject.getName());
+                return new ResponseEntity<Response>(new Response(projectRepository.save(project)), HttpStatus.OK);
+            }).orElseGet(() -> {
+                return new ResponseEntity<Response>(new Response("Project Not found of don't have access with given id.", null), HttpStatus.OK);
+            });
+        }
     }
 
     @RequestMapping("/{id}")
-    public ResponseEntity<Optional<Project>> edit(@PathVariable("id") Long id) {
-        return new ResponseEntity<Optional<Project>>(projectRepository.findById(id), HttpStatus.OK);
+    public ResponseEntity<Response> edit(@PathVariable("id") Long id) {
+        try {
+            Optional<Project> project;
+            if(Util.hasRole("ROLE_ADMIN")) {
+                project =  projectRepository.findById(id);
+            } else {
+                project =  projectRepository.findOwnProjectById(id, Util.getCurrentUsername());
+            }
+            if(project.isEmpty()) {
+                return new ResponseEntity<Response>(new Response("Object with given id Not Found or don't have access", null), HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<Response>(new Response(project.get()), HttpStatus.OK);
+            }
+        } catch (Exception ex) {
+            return new ResponseEntity<Response>(new Response("Failed to get Object", null), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+    public ResponseEntity<Response> delete(@PathVariable("id") Long id) {
         try {
-            Optional<Project> project =  projectRepository.findById(id);
+            Optional<Project> project;
+            if(Util.hasRole("ROLE_ADMIN")) {
+                project =  projectRepository.findById(id);
+            } else {
+                project =  projectRepository.findOwnProjectById(id, Util.getCurrentUsername());
+            }
             if(project.isEmpty()) {
-                return new ResponseEntity<String>("Object with given id Not Found", HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<Response>(new Response("Object with given id Not Found or don't have access", null), HttpStatus.BAD_REQUEST);
             } else {
                 projectRepository.delete(project.get());
             }
-            return new ResponseEntity<String>("Project With given id Deleted Successfully", HttpStatus.OK);
+            return new ResponseEntity<Response>(new Response(project.get()), HttpStatus.OK);
         } catch (Exception ex) {
-            return new ResponseEntity<String>("Failed to delete", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<Response>(new Response("Failed to delete", null), HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping("/find-all")
-    public ResponseEntity<List<Project>> findAll() {
-        List<Project> projectList = (List<Project>) projectRepository.findAll();
-        return new ResponseEntity<List<Project>>(projectList, HttpStatus.OK);
+    public ResponseEntity<Response> findAll() {
+        List<Project> projectList;
+        if(Util.hasRole("ROLE_ADMIN")) {
+            projectList =  (List<Project>) projectRepository.findAll();
+        } else {
+            projectList =  (List<Project>) projectRepository.findAllByUserName(Util.getCurrentUsername());
+        }
+        return new ResponseEntity<Response>(new Response(projectList), HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping("/find-all/by-user/{username}")
-    public ResponseEntity<List<Project>> findAllByUser(@PathVariable("username") String username) {
-        List<Project> projectList = (List<Project>) projectRepository.findAll();
-        return new ResponseEntity<List<Project>>(projectList, HttpStatus.OK);
+    public ResponseEntity<Response> findAllByUser(@PathVariable("username") String username) {
+        List<Project> projectList = (List<Project>) projectRepository.findAllByUserName(username);
+        return new ResponseEntity<Response>(new Response(projectList), HttpStatus.OK);
     }
 
 }
